750382620d720075f52a74d162c6ccd55d36330c
anuj <anujdixithbti@gmail.com>
2018-07-17 09:16:17 +0530

    Users add component

--- front-end/src/app/groups/groups.module.ts
+++ front-end/src/app/groups/groups.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
-
 import { GroupsRoutingModule } from './groups-routing.module';
 import { HomeComponent } from './home/home.component';
 import { CreateComponent as CreateComponentDialog} from './create/create.component';
@@ -8,15 +7,23 @@ import { EditComponent } from './edit/edit.component';
 import { ListComponent } from './list/list.component';
 import { ViewComponent } from './view/view.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { AddUsersComponent as AddUsersComponentDialog } from './add-users/add-users.component';
 
 @NgModule({
   imports: [
     SharedModule,
     GroupsRoutingModule
   ],
-  declarations: [HomeComponent, CreateComponentDialog, EditComponent, ListComponent, ViewComponent, UserListComponent],
+  declarations: [HomeComponent, 
+                 CreateComponentDialog, 
+                 EditComponent, 
+                 ListComponent, 
+                 ViewComponent, 
+                 UserListComponent, 
+                 AddUsersComponentDialog],
   entryComponents: [
-    CreateComponentDialog
+    CreateComponentDialog,
+    AddUsersComponentDialog
   ]
 })
 export class GroupsModule { }
--- front-end/src/app/groups/user-list/user-list.component.ts
+++ front-end/src/app/groups/user-list/user-list.component.ts
@@ -7,7 +7,8 @@ import { Subscription } from 'rxjs';
 import { switchMap, map } from 'rxjs/operators';
 import { User } from '../../shared/models/user.model';
 import { SelectionModel } from '@angular/cdk/collections';
-
+import { UiService } from '../../common/ui.service';
+import { AddUsersComponent as AddUsersComponentDialog } from '../add-users/add-users.component';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
@@ -19,18 +20,25 @@ export class UserListComponent implements OnInit, OnDestroy {
   private sub: Subscription = new Subscription();
   searchField: FormControl;
   searchForm: FormGroup;
+  selection = new SelectionModel<User>(true, []);
   displayedColumns: string[] = ['select', 'position', 'name', 'email', 'actions' ];
   
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor(private api: ApiService,
               private route: ActivatedRoute,
-              private fb: FormBuilder) { 
+              private fb: FormBuilder,
+              private uiService: UiService,
+              public dialog: MatDialog) { 
     this.searchField = new FormControl();
     this.searchForm = this.fb.group({searchField: this.searchField});     
   }
 
   ngOnInit() {
+   this.getUsers();    
+  }
+
+  getUsers() {
     this.sub = this.route.params.pipe(
       map(params => params['groupId']),
       switchMap(id => this.api.get(`admin/groups/${id}/users`))
@@ -42,11 +50,61 @@ export class UserListComponent implements OnInit, OnDestroy {
         },  
         errMsg => console.log(errMsg) 
     )
-    
   }
   
   ngOnDestroy() {
     this.sub.unsubscribe();
   }
+
+  deleteSelection(){
+    const selectedIds = this.selection.selected.map(user => user.id);
+    
+    if (selectedIds.length < 1) {
+      this.uiService.showToast("You have not made any selection", 'Close');
+    } else {
+      if(confirm('Are you sure you want to remove selected users from this group?')){
+        const groupId = this.route.snapshot.params['groupId']
+        this.api.post(`admin/groups/${groupId}/users/delete_all`, selectedIds)
+          .subscribe(
+            resp => {  
+              this.uiService.showToast("User Removed Successfully", 'Close');
+              this.getUsers();
+            },
+            errMsg => {
+              console.log(errMsg)
+            }
+          );
+      }
+    }
+  }
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.users.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.users.data.forEach(row => this.selection.select(row));
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AddUsersComponentDialog, {
+      width: '500px'      
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result === "saved") {
+        this.uiService.showToast("User added to Group Successfully", 'Close');
+        this.getUsers();
+        
+      }
+    });
+  }
+  
+
 
 }
