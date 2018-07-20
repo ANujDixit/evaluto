34052f8bb4dc2e972ec256a6583a62011712a6e6
Anuj <anytimetests@yahoo.com>
2018-07-20 06:30:37 +0000

    UAM Dashboard removal

--- front-end/src/app/uam/uam-routing.module.ts
+++ front-end/src/app/uam/uam-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent as UamHomeComponent } from './home/home.component';
 import { UamComponent } from './uam/uam.component';
 
 const routes: Routes = [
   { path: '', 
     component: UamComponent, 
     children: [
-      { path: '', redirectTo: '/admin/uam/home', pathMatch: 'full' },
-      { path: 'home', component: UamHomeComponent },       
+      { path: '', redirectTo: '/admin/uam/users/list', pathMatch: 'full' },
       { path: 'users', loadChildren: '../users/users.module#UsersModule'},
       { path: 'groups', loadChildren: '../groups/groups.module#GroupsModule'},
     ] 
